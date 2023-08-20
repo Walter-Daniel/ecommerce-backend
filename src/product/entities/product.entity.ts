15899840c4613cf79db0f0bb00e8bdcfdc4f5b72
Walter <walterdcarrizo.19@gmail.com>
2023-08-19 22:01:34 -0300

    Se crearon las tablas decategory, purchase, purchaseProducts y relaciones

---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
+import { BaseEntity } from '../../config/base.entity';
+import { CategoryEntity } from '../../category/entities/category.entity';
+import { PurchaseProductEntity } from '../../custom/entities/purchase-products.entity';
+import { PurchaseEntity } from '../../purchase/entities/purchase.entity';
+
+@Entity({name:'product'})
+export class ProductEntity extends BaseEntity {
+
+    @Column()
+    productName!: string;
+
+    @Column()
+    description!: string;
+
+    @Column()
+    price!: number;
+
+    @Column({ default: true })
+    stock!:boolean
+
+    @ManyToOne(() => CategoryEntity, (category) => category.products)
+    @JoinColumn({ name:'category_id' })
+    category!: CategoryEntity;
+
+    @OneToMany(() => PurchaseProductEntity, (purchaseProduct) => purchaseProduct.product)
+    purchaseProduct!: PurchaseProductEntity[]
+}
