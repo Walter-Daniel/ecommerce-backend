8952d006a2ddb75d0def2c31715af199c89d5d97
Walter <walterdcarrizo.19@gmail.com>
2023-08-25 15:32:11 -0300

    Se agregaron manejos de errores en customer, category, purchase y purchase-product

--- src/customer/routes/customer.routes.ts
+++ src/customer/routes/customer.routes.ts
@@ -7,7 +7,7 @@ export class CustomerRouter extends BaseRouter<CustomerController>{
     }
 
     routes(): void {
-        this.router.get('/customers', (req, res) => this.controller.getCustomer(req, res));
+        this.router.get('/customers', (req, res) => this.controller.getCustomers(req, res));
         this.router.get('/customer/:id', (req, res) => this.controller.getCustomerById(req, res));
         this.router.post('/createCustomer', (req, res) => this.controller.createCustomer(req, res));
         this.router.put('/updateCustomer/:id', (req, res) => this.controller.updateCustomer(req, res));
