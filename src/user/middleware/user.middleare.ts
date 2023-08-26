5c4056f150623471e75cbaec47d28271ad227b7b
Walter <walterdcarrizo.19@gmail.com>
2023-08-26 17:23:02 -0300

    Se crea middleare en el genérico de rutas, user y category

---
+++ src/user/middleware/user.middleare.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from 'express';
+import { validate } from 'class-validator';
+import { UserDTO } from '../dto/user.dto';
+import { HttpResponse } from '../../shared/response/htttp.response';
+
+export class UserMiddleware {
+    constructor(private readonly httpResponse : HttpResponse = new HttpResponse()){}
+    userValidatior(req: Request, res:Response, next: NextFunction){
+        const {name, lastname, email, password, province, city, rol} = req.body;
+
+        const validator = new UserDTO();
+        validator.name = name;
+        validator.lastname = lastname;
+        validator.email = email;
+        validator.password = password;
+        validator.province = province;
+        validator.city = city;
+        validator.rol = rol;
+   
+        validate(validator).then((error) => {
+            if(error.length > 0) {
+                return this.httpResponse.Error(res, error);
+            }else {
+                next();
+            }
+        });
+    }
+}
